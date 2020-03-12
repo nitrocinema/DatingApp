06f9cbe33c7467b6601652a0a7c4e6b8338af7d6
bobandamljanovic <bobandamljanovic@live.com>
2020-03-12 11:16:51 +0100

    Initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingApp.API.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public string ime;
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values {get; set;}
+    }
+}
