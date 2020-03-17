bff26d15358098087a216c345e6aaad372cc43dc
bobandamljanovic <bobandamljanovic@live.com>
2020-03-17 12:12:56 +0100

    end of section 3

---
+++ DatingApp.API/models/User.cs
@@ -0,0 +1,11 @@
+namespace DatingApp.API.models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        public byte[] PaswordHash { get; set; }
+        public byte[] PaswordSalt { get; set; }
+    }
+}
